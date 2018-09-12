f123f7ab45bf45edc845761c2476cd12c37706ac
lexi-luo <40446351+lexi-luo@users.noreply.github.com>
2018-09-12 14:15:16 +1200

    Update Post.jsx

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-const Post = ({title}) => (
-  <div>{title}</div>
+const Post = ({title, created, thumbnail}) => {
+ let date = convertEpochToDateString(created)
+  return (
+  <div><h2>{title}</h2> <br>
+  </br> Date: {date} <br>
+  </br> <img src={thumbnail}/></div>
 )
+}
+
+const convertEpochToDateString = (epoch) => {
+  return (new Date(epoch * 1000)).toString()
+}
 
 export default Post
